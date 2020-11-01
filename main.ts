61defe1e86be3ea3c11d32d196bc93500ff5541d
Jack Munkonge <jackmunkonge@gmail.com>
2020-11-01 20:33:46 +0000

    Remove dev tools by default

--- main.ts
+++ main.ts
@@ -27,7 +27,7 @@ function createWindow(): BrowserWindow {
 
   if (serve) {
 
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 
     require('electron-reload')(__dirname, {
       electron: require(`${__dirname}/node_modules/electron`)
