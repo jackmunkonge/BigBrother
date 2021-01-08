3e9afabb65e7e58f7c0a7ea138bc4608251d6310
Jack Munkonge <jackmunkonge@gmail.com>
2021-01-08 15:28:56 +0000

    Add basic contacts CRUD (no persist)

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BreadcrumbService } from '../../app.breadcrumb.service';
-import { Product } from '../../demo/domain/product';
-import { ProductService } from '../../demo/service/productservice';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { Contact } from '../../models/contact';
+import { ContactsService } from '../../services/contacts.service';
 
 @Component({
   templateUrl: './settings.component.html',
@@ -11,19 +11,16 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 })
 export class SettingsComponent implements OnInit {
 
-  productDialog: boolean;
-
-  products: Product[];
-
-  product: Product;
-
-  selectedProducts: Product[];
+  contactDialog: boolean;
+  contacts: Contact[];
+  contact: Contact;
+  selectedContacts: Contact[];
 
   submitted: boolean;
 
   cols: any[];
 
-  constructor(private productService: ProductService, private messageService: MessageService,
+  constructor(private contactsService: ContactsService, private messageService: MessageService,
               private confirmationService: ConfirmationService, private breadcrumbService: BreadcrumbService) {
     this.breadcrumbService.setItems([
       {label: 'Settings'}
@@ -31,84 +28,82 @@ export class SettingsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.productService.getProducts().then(data => this.products = data);
+    this.contactsService.getContacts()
+      .then(data => this.contacts = data);
 
     this.cols = [
-      { field: 'name', header: 'Name' },
-      { field: 'price', header: 'Price' },
-      { field: 'category', header: 'Category' },
-      { field: 'rating', header: 'Reviews' },
-      { field: 'inventoryStatus', header: 'Status' }
+      {field: 'name', header: 'Name'},
+      {field: 'number', header: 'Phone Number'},
+      {field: 'lastCalled', header: 'Last Called'}
     ];
   }
 
   openNew() {
-    this.product = {};
+    this.contact = {};
     this.submitted = false;
-    this.productDialog = true;
+    this.contactDialog = true;
   }
 
-  deleteSelectedProducts() {
+  deleteSelectedContacts() {
     this.confirmationService.confirm({
-      message: 'Are you sure you want to delete the selected products?',
+      message: 'Are you sure you want to delete the selected contacts?',
       header: 'Confirm',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-        this.products = this.products.filter(val => !this.selectedProducts.includes(val));
-        this.selectedProducts = null;
-        this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000});
+        this.contacts = this.contacts.filter(val => !this.selectedContacts.includes(val));
+        this.selectedContacts = null;
+        this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Contacts Deleted', life: 3000});
       }
     });
   }
 
-  editProduct(product: Product) {
-    this.product = {...product};
-    this.productDialog = true;
+  editContact(contact: Contact) {
+    this.contact = {...contact};
+    this.contactDialog = true;
   }
 
-  deleteProduct(product: Product) {
+  deleteContact(contact: Contact) {
     this.confirmationService.confirm({
-      message: 'Are you sure you want to delete ' + product.name + '?',
+      message: 'Are you sure you want to delete ' + contact.name + '?',
       header: 'Confirm',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-        this.products = this.products.filter(val => val.id !== product.id);
-        this.product = {};
-        this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000});
+        this.contacts = this.contacts.filter(val => val.id !== contact.id);
+        this.contact = {};
+        this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Contact Deleted', life: 3000});
       }
     });
   }
 
   hideDialog() {
-    this.productDialog = false;
+    this.contactDialog = false;
     this.submitted = false;
   }
 
-  saveProduct() {
+  saveContact() {
     this.submitted = true;
 
-    if (this.product.name.trim()) {
-      if (this.product.id) {
-        this.products[this.findIndexById(this.product.id)] = this.product;
-        this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
+    if (this.contact.name.trim()) {
+      if (this.contact.id) {
+        this.contacts[this.findIndexById(this.contact.id)] = this.contact;
+        this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Contact Updated', life: 3000});
       }
       else {
-        this.product.id = this.createId();
-        this.product.image = 'product-placeholder.svg';
-        this.products.push(this.product);
-        this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000});
+        this.contact.id = this.createId();
+        this.contacts.push(this.contact);
+        this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Contact Created', life: 3000});
       }
 
-      this.products = [...this.products];
-      this.productDialog = false;
-      this.product = {};
+      this.contacts = [...this.contacts];
+      this.contactDialog = false;
+      this.contact = {};
     }
   }
 
   findIndexById(id: string): number {
     let index = -1;
-    for (let i = 0; i < this.products.length; i++) {
-      if (this.products[i].id === id) {
+    for (let i = 0; i < this.contacts.length; i++) {
+      if (this.contacts[i].id === id) {
         index = i;
         break;
       }
